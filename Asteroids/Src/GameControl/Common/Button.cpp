4315483f866d4b59014f93e1826d8a5230b20a29
Jack Clements <frightmare.wow@hotmail.co.uk>
2021-04-12 14:20:03 +0100

    added docs for button

--- Asteroids/Src/GameControl/Common/Button.cpp
+++ Asteroids/Src/GameControl/Common/Button.cpp
@@ -1,5 +1,13 @@
 #include "Button.h"
 
+/* Constructor
+*  Brief:
+*  assigns references of the desired font and texture to be used
+*  for the button.
+*  References are used as this class depends upon these parameters
+*  and thus share a common lifecycle. Behaviour for deleted references
+*  is undefined.
+*/
 Button::Button(sf::Font& textFont, sf::Texture& buttonTexture)
 	:
 	_buttonFont(textFont),
@@ -9,6 +17,7 @@ Button::Button(sf::Font& textFont, sf::Texture& buttonTexture)
 	this->_button.setTexture(_buttonTexture);
 }
 
+// Overloaded constructor to also take in the text string at assignment
 Button::Button(sf::Font& textFont, sf::Texture& buttonTexture, std::string buttonText)
 	:
 	Button(textFont, buttonTexture)
@@ -16,6 +25,13 @@ Button::Button(sf::Font& textFont, sf::Texture& buttonTexture, std::string butto
 	this->_buttonText.setString(buttonText);
 }
 
+/* void SetPosition
+* Brief:
+*	Sets the position of the outer button and calculates
+*	the center position for the text accordingly.
+* Params:
+* <const vector2f> position - position to assign to the button
+*/
 void Button::SetPosition(const sf::Vector2f position)
 {
 	// Set the position of the button sprite
@@ -27,28 +43,62 @@ void Button::SetPosition(const sf::Vector2f position)
 		(_button.getGlobalBounds().height / 2) - (_buttonText.getGlobalBounds().height / 2) });
 }
 
+/* void SetButtonColor
+* Brief:
+*	sets the color of the outer button.
+* Params:
+* <const color> color- color to assign to the outer button texture
+*/
 void Button::SetButtonColor(const sf::Color color)
 {
 	this->_button.setColor(color);
 }
 
+/* void SetButtonTextColor
+* Brief:
+*	sets the color of the button text
+* Params:
+* <const color> color - color to assign to the text
+*/
 void Button::SetButtonTextColor(const sf::Color color)
 {
 	this->_defaultTextColor = color;
 }
 
+/* void SetScale
+* Brief:
+*	sets the scale of both the button sprite and the text
+*	within it
+* Params:
+*	 <const float> scale - scale to set to the button
+*/
 void Button::SetScale(const float scale)
 {
 	this->_button.setScale({ scale, scale });
 	this->_buttonText.setScale({ scale, scale});
 }
 
+/* void Draw
+* Brief:
+*	sets the color of the outer button.
+* Params:
+* <const color> color- color to assign to the outer button texture
+*/
 void Button::Draw(sf::RenderWindow* window)
 {
 	window->draw(this->_button);
 	window->draw(this->_buttonText);
 }
 
+/* void Update
+* Brief:
+*	Checks to see if the mouse position is within the bounds of the button.
+*	if this is the case it will hilight the text within the button.
+*	if a mouse click is detected while inside the button this method will return true
+*	otherwise false.
+* Params:
+* <renderwindow*>	targetWindow	-	window in which the button and mouse is contained
+*/
 bool Button::Update(sf::RenderWindow* targetWindow)
 {
 	// check if mouse is on button
@@ -70,6 +120,7 @@ bool Button::Update(sf::RenderWindow* targetWindow)
 	return false;
 }
 
+// Getter for the global bounds of the button sprite
 sf::FloatRect Button::GetGlobalBounds()
 {
 	return this->_button.getGlobalBounds();
--- Asteroids/Src/GameControl/Common/Button.h
+++ Asteroids/Src/GameControl/Common/Button.h
@@ -1,29 +1,89 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
+/* Class Button
+ * Creates a simple button with a passed texture and font 
+ */
 class Button
 {
 public:
+	/* Constructor
+	*  Brief:
+	*  assigns references of the desired font and texture to be used
+	*  for the button.
+	*  References are used as this class depends upon these parameters
+	*  and thus share a common lifecycle. Behaviour for deleted references
+	*  is undefined.
+	*/
 	Button(sf::Font& textFont, sf::Texture& buttonTexture);
+
+	// Overloaded constructor to also take in the text string at assignment
 	Button(sf::Font& textFont, sf::Texture& buttonTexture, std::string buttonText);
 
+	/* void SetPosition
+	* Brief:
+	*	Sets the position of the outer button and calculates
+	*	the center position for the text accordingly.
+	* Params:
+	* <const vector2f> position - position to assign to the button
+	*/
 	void SetPosition(const sf::Vector2f position);
+
+	/* void SetButtonColor
+	* Brief:
+	*	sets the color of the outer button.
+	* Params:
+	* <const color> color- color to assign to the outer button texture
+	*/
 	void SetButtonColor(const sf::Color color);
+
+	/* void SetButtonTextColor
+	* Brief:
+	*	sets the color of the button text
+	* Params:
+	* <const color> color - color to assign to the text
+	*/
 	void SetButtonTextColor(const sf::Color color);
+
+	/* void SetScale
+	* Brief:
+	*	sets the scale of both the button sprite and the text
+	*	within it
+	* Params:
+	*	 <const float> scale - scale to set to the button
+	*/
 	void SetScale(const float scale);
 
+	/* void Draw
+	* Brief:
+	*	sets the color of the outer button.
+	* Params:
+	* <const color> color- color to assign to the outer button texture
+	*/
 	void Draw(sf::RenderWindow* window);
 
+	/* void Update
+	* Brief:
+	*	Checks to see if the mouse position is within the bounds of the button.
+	*	if this is the case it will hilight the text within the button.
+	*	if a mouse click is detected while inside the button this method will return true
+	*	otherwise false.
+	* Params:
+	* <renderwindow*>	targetWindow	-	window in which the button and mouse is contained
+	*/
 	bool Update(sf::RenderWindow* targetWindow);
 
+	// Getter for the global bounds of the button sprite
 	sf::FloatRect GetGlobalBounds();
-private:
-	sf::Font& _buttonFont;
-	sf::Text _buttonText;
-	sf::Texture& _buttonTexture;
-	sf::Sprite _button;
 
-	sf::Color _defaultTextColor = sf::Color::White;
-	sf::Color hoverColor = sf::Color::Red;
+	// Private members
+private:
+	sf::Font& _buttonFont;								// Font of the button text
+	sf::Text _buttonText;								// The object contained within the button
+	sf::Texture& _buttonTexture;						// Texture of the button sprite
+	sf::Sprite _button;									// sprite which holds the global bounds
+
+	sf::Color _defaultTextColor = sf::Color::White;		// default colour of the button text
+	sf::Color hoverColor = sf::Color::Red;				// highlight button text colour
 };
 
